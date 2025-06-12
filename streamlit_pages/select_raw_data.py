83e7b82df0322e0fe6b2cf90e1ee0dcf6c657287
CyprienBouton <boutoncyprien@yahoo.com>
2025-06-12 14:28:29 +0200

    [add] buffer_file and image_buffer in session_sate in select_raw_data

--- streamlit_pages/select_raw_data.py
+++ streamlit_pages/select_raw_data.py
@@ -11,14 +11,17 @@ def select_raw_data():
     uploaded_file = st.file_uploader("Upload your .dat Twix file", type=["dat"])
     if uploaded_file is not None:
         with st.spinner("Reading Twix file..."):
+            st.session_state.buffer_file = uploaded_file.getbuffer()
             with open("uploaded_file.dat", "wb") as f:
-                f.write(uploaded_file.getbuffer())
+                f.write(st.session_state.buffer_file)
             try:
                 twix = twixtools.read_twix("uploaded_file.dat")
                 st.session_state.twix = twix
                 st.success("File loaded!")
                 st.session_state.df = build_line_dataframe(twix)
                 st.session_state.file = os.path.basename(uploaded_file.name)
+                st.session_state.img_nii = None
+                st.session_state.image_buffer = None
                 os.remove("uploaded_file.dat")
             except Exception as e:
                 st.error(f"Failed to read twix file: {e}")
