fcb2f9cbd538f1f75857baa36117f584a0baaf49
CyprienBouton <boutoncyprien@yahoo.com>
2025-07-17 14:10:15 +0200

    [add] get_trigger_timing in utils/twix_dataframe.py to get the timings of the RR events

--- utils/twix_dataframe.py
+++ utils/twix_dataframe.py
@@ -21,10 +21,31 @@ def build_line_dataframe(twix, trigger_method='ECG1'):
     if 'pmu' in twix[-1] and any(twix[-1]['pmu'].trigger[trigger_method]):
         pmu = twix[-1]['pmu']
         mask = pmu.trigger[trigger_method]>0
-        trigger_timing = pmu.timestamp_trigger[trigger_method][mask]
-        trigger_timing = (trigger_timing - start_time)  * 2.5e-3 # convert to seconds
+        trigger_timing = get_trigger_timing(twix, trigger_method)
         idxs_sorted = np.searchsorted(trigger_timing, timestamps)
         RDs = np.diff(trigger_timing)[idxs_sorted-2] # last trigger - previous trigger
         df['RD'] =  np.round(RDs, 2)
     
     return df
+
+
+def get_trigger_timing(twix, trigger_method='ECG1'):
+    """
+    Get trigger timing from the twix data.
+
+    Parameters:
+    - twix: The twix data structure.
+    - trigger_method: The method used to trigger the acquisition.
+
+    Returns:
+    - Array of trigger timings.
+    """
+    assert 'pmu' in twix[-1] and any(twix[-1]['pmu'].trigger[trigger_method]), \
+        f"No PMU data found for trigger method '{trigger_method}'."
+    pmu = twix[-1]['pmu']
+    start_time = twix[-1]['mdb'][0].mdh.TimeStamp
+    mask = pmu.trigger[trigger_method]>0
+    trigger_timing = pmu.timestamp_trigger[trigger_method][mask]
+    trigger_timing = (trigger_timing - start_time)  * 2.5e-3 # convert to seconds
+
+    return trigger_timing
